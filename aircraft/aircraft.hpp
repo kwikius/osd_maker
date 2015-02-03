d0b147e787d5ccc7dafd622afb7f2bb93b298068
skyscraper <kwikius@yahoo.com>
2015-02-03 16:54:18 +0000

    remove graphics api impl, will put in quan in two versions, pc and board one

--- aircraft/aircraft.hpp
+++ aircraft/aircraft.hpp
@@ -27,100 +27,44 @@
 #include <quan/charge.hpp>
 #include <quan/angle.hpp>
 #include <quan/uav/position.hpp>
-
-
-/*
-See http://gentlenav/googlecode.com/files/DCMdraft2.pdf 
-
-"The orientation of the body frame with respect to the fixed earth system
-can be determined in the following manner. Imagine the aircraft to be 
-positioned so that the body axis system is parallel to the fixed frame
-and then apply the following rotations:
-1. Rotate the body about its zbody axis through the yaw angle thetaZ
-2. Rotate the body about its ybody axis through the pitch angle thetaY
-3. Rotate the body about its xbody axis through the roll angle thetaX 
-
-The rotation matrix R =   // (untested!)
- [ 
-  cos(thetaY) * cos(thetaZ) , 
-      sin(thetaX) * sin(thetaY) * cos(thetaZ) - cos(thetaX) * sin(thetaZ),
-         cos(thetaX) * sin(thetaY) * cos(thetaZ) - sin(thetaX) * sin(thetaZ)
-   cos(thetaY) * sin(thetaZ),
-      sin(thetaX) * sin(thetaY) * sin(thetaZ) + cos(thetaX) * cos(thetaZ),
-         cos(thetaX) * sin(thetaY) * sin(thetaZ) - sin(thetaX) * cos(thetaZ)
-   -sin(thetaY),
-      sin(thetaX) * cos(thetaY),
-         cos(thetaX) * cos(thetaY)
-]
-*/
-
-struct attitude_t{
-   
-   quan::angle_<float>::deg            pitch;                  
-   quan::angle_<float>::deg            roll;                   
-   quan::angle_<float>::deg            yaw; 
-   attitude_t():pitch{0},roll{0},yaw{0}{}
-};
-
-// use for array of batteries
-struct battery{
-   battery(std::string const & name,quan::charge_<float>::mA_h const & capacity)
-   : m_name{name}, m_capacity{capacity}
-   {}
-   std::string const & get_name() const{return m_name;}
-   quan::voltage_<float>::V get_voltage() const { return m_voltage;}
-   quan::current::A get_current() const { return m_current;}
-   quan::charge::mA_h get_charge_remaining() const{ return m_charge_remaining;}
-   quan::charge::mA_h get_capacity()const{return m_capacity;}
-   
-   
-   private:
-   std::string                         m_name;
-   quan::voltage_<float>::V            m_voltage;
-   quan::current_<float>::A            m_current;
-   quan::charge_<float>::mA_h          m_charge_remaining;
-   quan::charge_<float>::mA_h const    m_capacity;
-};
+#include <quan/uav/attitude.hpp>
 
 struct aircraft{
-   typedef quan::angle_<int32_t>::deg10e7 lat_lon_type;
-   typedef quan::length_<float>::mm        altitude_type;
-   typedef quan::uav::position<lat_lon_type,altitude_type> position_type;
+   typedef quan::angle_<int32_t>::deg10e7                   lat_lon_type;
+   typedef quan::length_<float>::mm                         altitude_type;
+   typedef quan::uav::position<lat_lon_type,altitude_type>  position_type;
 
    position_type const & get_position() const { return m_position;}
    position_type const & get_home_position() const { return m_home_position;}
-   attitude_t const & get_attitude() const { return m_attitude;}
+   quan::uav::attitude<float> const & get_attitude() const { return m_attitude;}
    quan::angle_<float>::deg const & get_heading()const { return m_heading;}
-   void set_heading(quan::angle_<float>::deg const & value_in)
-   {
-      auto value = value_in;
-      while( value > quan::angle::deg{360}){
-         value -= quan::angle::deg{360};
-      }
-      while( value < quan::angle::deg{0}){
-         value += quan::angle::deg{360};
-      }
-      this->m_heading = value;
-   }
+   quan::velocity_<float>::m_per_s const & get_airspeed()const { return m_airspeed;}
+   quan::velocity_<float>::m_per_s const & get_groundspeed()const { return m_groundspeed;}
+   // bool hav_airspeed
+//   void set_heading(quan::angle_<float>::deg const & value_in)
+//   {
+//      auto value = value_in;
+//      while( value > quan::angle::deg{360}){
+//         value -= quan::angle::deg{360};
+//      }
+//      while( value < quan::angle::deg{0}){
+//         value += quan::angle::deg{360};
+//      }
+//      this->m_heading = value;
+//   }
    private:
    position_type                       m_position;
    position_type                       m_home_position;
-   attitude_t                          m_attitude;
-     
+   quan::uav::attitude<float>          m_attitude;
    quan::angle_<float>::deg            m_heading;              
    quan::velocity_<float>::m_per_s     m_airspeed;            
-   quan::velocity_<float>::m_per_s     m_groundspeed;
-
-      // ADD rc output values              
+   quan::velocity_<float>::m_per_s     m_groundspeed;             
   public:               
    aircraft():
-     // throttle{0},
       m_heading{0}
       ,m_airspeed{0}
       ,m_groundspeed{0}
    {}
-
-   private:
 
 };
 
